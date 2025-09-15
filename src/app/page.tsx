b3b0857e2cf4bcbdd89dbf4ba9a374e0373b24bb
Abdul Rehman <abdulrehmanbhatti976@gmail.com>
2025-09-15 23:54:57 +0500

    abdul1/100

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
          aur log mehmaan-nawazi ke liye mashhoor hain.
       </p>
       <img 
-        src="lalian.jpg" 
-        alt="lalian image" 
+        src="hero.jpg" 
+        alt="hero image" 
         style={{ width: "200px", height: "auto" }} 
       />
     </section>
